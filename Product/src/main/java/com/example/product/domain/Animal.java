8c5ac11cf4593168db76fe55be68fc408f37b50a
deeredge <deeredge@qq.com>
2021-12-13 17:39:44 +0800

    System.out.println("I am an animal,please protect me2!");

--- Product/src/main/java/com/example/product/domain/Animal.java
+++ Product/src/main/java/com/example/product/domain/Animal.java
@@ -3,5 +3,6 @@ package com.example.product.domain;
 public class Animal {
     public void eat(){
         System.out.println("I am an animal,please protect me!");
+        System.out.println("I am an animal,please protect me2!");
     }
 }
